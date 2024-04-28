e681d379219175562a906f23bf50280a0876de09
isletspace <liewzheng@qq.com>
2024-04-28 11:44:29 +0800

    work successfully

--- index.js
+++ index.js
@@ -344,29 +344,127 @@ function update_python_code() {
  */
 function update_reg_0x00() {
 
-    newText = ``;
+    document.getElementById('reg_0x00').innerHTML = "";
 
-    reg_0x00_reset = (reg_0x00 >> 15 == 1) ? true : false;
-    newText += `- (RST) Reset: ${reg_0x00_reset}<br>`
+    var reg_0x00_reset_div = document.createElement('div');
+    reg_0x00_reset_div.className = 'reg_0x00_bit';
 
+    var reg_0x00_reset_label = document.createElement('span');
+    reg_0x00_reset_div.appendChild(reg_0x00_reset_label);
+    reg_0x00_reset_label.innerHTML = '- (RST) Power on Reset: ';
+    reg_0x00_reset_label.style.fontWeight = 'bold';
+
+    var reg_0x00_reset_value = document.createElement('span');
+    reg_0x00_reset_div.appendChild(reg_0x00_reset_value);
+    reg_0x00_reset = (reg_0x00 >> 15 == 1) ? "Enable" : "Disable";
+    reg_0x00_reset_value.innerHTML = reg_0x00_reset;
+    reg_0x00_reset_value.style.color = reg_0x00_reset ? 'green' : 'red';
+
+    document.getElementById('reg_0x00').appendChild(reg_0x00_reset_div);
+
+    var reg_0x00_brng_div = document.createElement('div');
+    reg_0x00_brng_div.className = 'reg_0x00_bit';
+
+    var reg_0x00_brng_label = document.createElement('span');
+    reg_0x00_brng_div.appendChild(reg_0x00_brng_label);
+    reg_0x00_brng_label.innerHTML = '- (BRNG) Bus Voltage Range: ';
+    reg_0x00_brng_label.style.fontWeight = 'bold';
+
+    var reg_0x00_brng_value = document.createElement('span');
+    reg_0x00_brng_div.appendChild(reg_0x00_brng_value);
     reg_0x00_brng = (( (reg_0x00 & 0x2000) >> 13) == 1) ? "32V" : "16V";
-    newText += `- (BRNG) Bus Voltage Range: ${reg_0x00_brng}<br>`;
+    reg_0x00_brng_value.innerHTML = reg_0x00_brng;
 
+    document.getElementById('reg_0x00').appendChild(reg_0x00_brng_div);
+
+    var reg_0x00_pga_gain_div = document.createElement('div');
+    reg_0x00_pga_gain_div.className = 'reg_0x00_bit';
+
+    var reg_0x00_pga_gain_label = document.createElement('span');
+    reg_0x00_pga_gain_div.appendChild(reg_0x00_pga_gain_label);
+    reg_0x00_pga_gain_label.innerHTML = '- (PGA) Gain: ';
+    reg_0x00_pga_gain_label.style.fontWeight = 'bold';
+
+    var reg_0x00_pga_gain_value = document.createElement('span');
+    reg_0x00_pga_gain_div.appendChild(reg_0x00_pga_gain_value);
     reg_0x00_pga_gain = Math.pow(2, ((reg_0x00 & 0x1800) >> 11));
+    reg_0x00_pga_gain_value.innerHTML = reg_0x00_pga_gain;
+
+    document.getElementById('reg_0x00').appendChild(reg_0x00_pga_gain_div);
+
+    var reg_0x00_pga_range_div = document.createElement('div');
+    reg_0x00_pga_range_div.className = 'reg_0x00_bit';
+
+    var reg_0x00_pga_range_label = document.createElement('span');
+    reg_0x00_pga_range_div.appendChild(reg_0x00_pga_range_label);
+    reg_0x00_pga_range_label.innerHTML = '- (PGA) Range: ';
+    reg_0x00_pga_range_label.style.fontWeight = 'bold';
+
+    var reg_0x00_pga_range_value = document.createElement('span');
+    reg_0x00_pga_range_div.appendChild(reg_0x00_pga_range_value);
     reg_0x00_pga_range = reg_0x00_pga_gain * 40;
-    newText += `- PGA Gain: ${reg_0x00_pga_gain}<br> - PGA Range: +/-${reg_0x00_pga_range}mV<br>`;
+    reg_0x00_pga_range_value.innerHTML = reg_0x00_pga_range;
 
-    newText += `- BADC:<br>`;
+    document.getElementById('reg_0x00').appendChild(reg_0x00_pga_range_div);
     
-    newText += `- SADC:<br>`;
+    var reg_0x00_badc_div = document.createElement('div');
+    reg_0x00_badc_div.className = 'reg_0x00_bit';
 
+    var reg_0x00_badc_label = document.createElement('span');
+    reg_0x00_badc_div.appendChild(reg_0x00_badc_label);
+    reg_0x00_badc_label.innerHTML = '- (BADC) Bus ADC Resolution: ';
+    reg_0x00_badc_label.style.fontWeight = 'bold';
+
+    var reg_0x00_badc_value = document.createElement('span');
+    reg_0x00_badc_div.appendChild(reg_0x00_badc_value);
+    reg_0x00_badc = (reg_0x00 & 0x0780) >> 7;
+    reg_0x00_badc_value.innerHTML = reg_0x00_badc;
+
+    document.getElementById('reg_0x00').appendChild(reg_0x00_badc_div);
+
+    var reg_0x00_sadc_div = document.createElement('div');
+    reg_0x00_sadc_div.className = 'reg_0x00_bit';
+
+    var reg_0x00_sadc_label = document.createElement('span');
+    reg_0x00_sadc_div.appendChild(reg_0x00_sadc_label);
+    reg_0x00_sadc_label.innerHTML = '- (SADC) Shunt ADC Resolution: ';
+    reg_0x00_sadc_label.style.fontWeight = 'bold';
+
+    var reg_0x00_sadc_value = document.createElement('span');
+    reg_0x00_sadc_div.appendChild(reg_0x00_sadc_value);
+    reg_0x00_sadc = (reg_0x00 & 0x0078) >> 3;
+    reg_0x00_sadc_value.innerHTML = reg_0x00_sadc;
+
+    document.getElementById('reg_0x00').appendChild(reg_0x00_sadc_div);
+
+    var reg_0x00_mode_div = document.createElement('div');
+    reg_0x00_mode_div.className = 'reg_0x00_bit';
+
+    var reg_0x00_mode_label = document.createElement('span');
+    reg_0x00_mode_div.appendChild(reg_0x00_mode_label);
+    reg_0x00_mode_label.innerHTML = '- (MODE) Operating Mode: ';
+    reg_0x00_mode_label.style.fontWeight = 'bold';
+
+    var reg_0x00_mode_value = document.createElement('span');
+    reg_0x00_mode_div.appendChild(reg_0x00_mode_value);
     reg_0x00_mode = ina219_mode_dict['mode'+ (reg_0x00 & 0x0007)];
-    newText += `- Mode: ${reg_0x00_mode}<br>`;
+    reg_0x00_mode_value.innerHTML = reg_0x00_mode;
 
+    document.getElementById('reg_0x00').appendChild(reg_0x00_mode_div);
 
-    newText += `<br>Value of 0x00 register: 0x${reg_0x00.toString(16).toUpperCase()}`;
+    var reg_0x00_value_div = document.createElement('div');
 
-    document.getElementById('reg_0x00').innerHTML = newText;
+    var reg_0x00_value_label = document.createElement('span');
+    reg_0x00_value_div.appendChild(reg_0x00_value_label);
+    reg_0x00_value_label.innerHTML = 'Value of 0x00 register: ';
+    reg_0x00_value_label.style.fontWeight = 'bold';
+
+    var reg_0x00_value_value = document.createElement('span');
+    reg_0x00_value_div.appendChild(reg_0x00_value_value);
+    reg_0x00_value_value.innerHTML = `0x${reg_0x00.toString(16).toUpperCase()}`;
+
+    document.getElementById('reg_0x00').appendChild(reg_0x00_value_div);
+    
 }
 
 function initialize() {
@@ -377,6 +475,7 @@ function initialize() {
 
     update_formula();
     update_python_code();
+
 }
 
 /*-----------------------------------------------------------------------------
